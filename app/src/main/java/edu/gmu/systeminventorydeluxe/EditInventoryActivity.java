1dc788d1da726e613f04ffec4aa2afac4f6a3224
Isabelle Trenkova <izzytrenkov@gmail.com>
2022-03-12 16:37:10 -0500

    Adding Classes and XML for adding inventory function
    Added EditInventoryActivity.java
    InventoryMainActivity.java
    activity_edit_inventory.xml
    activity_inventory.xml

---
+++ app/src/main/java/edu/gmu/systeminventorydeluxe/EditInventoryActivity.java
@@ -0,0 +1,13 @@
+package edu.gmu.systeminventorydeluxe;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class EditInventoryActivity extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_inventory);
+}
---
+++ app/src/main/java/edu/gmu/systeminventorydeluxe/InventoryMainActivity.java
@@ -0,0 +1,14 @@
+package edu.gmu.systeminventorydeluxe;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class InventoryMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inventory);
+    }
+}
