ea07f1ffcf07f1c4aa5b1d757de4062f9f35e928
Isabelle Trenkova <izzytrenkov@gmail.com>
2022-03-15 12:24:47 -0400

    update activity_edit_invnetory.xml
    Added more functionality

--- app/src/main/java/edu/gmu/systeminventorydeluxe/EditInventoryActivity.java
+++ app/src/main/java/edu/gmu/systeminventorydeluxe/EditInventoryActivity.java
@@ -35,7 +35,7 @@ public class EditInventoryActivity extends AppCompatActivity implements
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_edit_inventory);
+        setContentView(R.layout.activity_edit_inventoryView);
 
         define();
     }
--- app/src/main/java/edu/gmu/systeminventorydeluxe/InventoryMainActivity.java
+++ app/src/main/java/edu/gmu/systeminventorydeluxe/InventoryMainActivity.java
@@ -13,6 +13,6 @@ public class InventoryMainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_inventory);
+        setContentView(R.layout.activity_inventory_view);
     }
 }
