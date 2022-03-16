57080fe43649b295dad077a9cf6b7a73a50424ed
Isabelle Trenkova <izzytrenkov@gmail.com>
2022-03-15 21:59:38 -0400

    Updated EditInventoryActivity.java
    Added some good stuff, mostly the define method, defining fields/input params. Also a very funky fresh dynamic header so the same page can be used to add and edit items.

--- app/src/main/java/edu/gmu/systeminventorydeluxe/EditInventoryActivity.java
+++ app/src/main/java/edu/gmu/systeminventorydeluxe/EditInventoryActivity.java
@@ -1,8 +1,11 @@
 package edu.gmu.systeminventorydeluxe;
 
+import android.content.Intent;
 import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -12,7 +15,12 @@ import androidx.loader.app.LoaderManager;
 import androidx.loader.content.Loader;
 
 /**
- * This will be the class where items are actually edited
+ * This will be the class where items are actually edited AND ADDED
+ *
+ * There will be a dynamic message stating that if the item is new
+ * as in the user is about to add it, then an add message will be displayed
+ * otherwise an edit message will be displayed.
+ *
  * From any action that will edit an item all activity should be redirected
  * here and to the Activity_edit_inventory to actually edit an item
  */
@@ -27,6 +35,15 @@ public class EditInventoryActivity extends AppCompatActivity implements
      */
     //private EditText itemWeight;
     private EditText itemDescription;
+    private ImageButton productImage;
+
+    //add recipe field right here and don't forget to include it in the
+    //define() method
+    //add functionality later on
+
+    //using intent class you can see if there had been any modifications
+    //more specifically getIntent(); if it is null then it is a new product
+    private Uri inventoryItemStatus;
 
     /**
      * Upon new instnace;
@@ -38,6 +55,8 @@ public class EditInventoryActivity extends AppCompatActivity implements
         setContentView(R.layout.activity_edit_inventoryView);
 
         define();
+
+
     }
 
 
@@ -47,6 +66,22 @@ public class EditInventoryActivity extends AppCompatActivity implements
     protected void define() {
 
         //have a dynamic way to set text, figure out
+
+        dynamicMessage = (TextView) findViewById(R.id.messageView);
+        nameItem = (EditText) findViewById(R.id.name_item);
+        itemQuantity = (EditText) findViewById(R.id.quantity_item);
+        itemDescription = (EditText) findViewById(R.id.decription_item);
+        productImage = (ImageButton) findViewById(R.id.product_image);
+
+        inventoryItemStatus = ((getIntent()).getData());
+
+        if (inventoryItemStatus != null) {
+
+            dynamicMessage.setText("Edit Item");
+        } else {
+
+            dynamicMessage.setText("Add Item");
+        }
 
     }
 
