cd126b701c180f82426e306ae741d2d9377ad13b
Isabelle Trenkova <izzytrenkov@gmail.com>
2022-05-03 19:35:14 -0400

    Update on testing file

--- app/src/androidTest/java/edu/gmu/systeminventorydeluxe/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/edu/gmu/systeminventorydeluxe/ExampleInstrumentedTest.java
@@ -3,66 +3,17 @@ package edu.gmu.systeminventorydeluxe;
 
 
 import static androidx.test.espresso.Espresso.onView;
-
-
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
-import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
-
-import org.junit.After;
-import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
-
-import static org.junit.Assert.*;
-
-import android.content.Context;
-import android.widget.TextView;
-
-
-import static android.app.Instrumentation.ActivityResult;
-import static androidx.test.espresso.Espresso.onView;
-import static androidx.test.espresso.action.ViewActions.click;
-import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
-import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
-import static androidx.test.espresso.intent.Intents.intended;
-import static androidx.test.espresso.intent.Intents.intending;
-import static androidx.test.espresso.intent.matcher.ComponentNameMatchers.hasShortClassName;
-import static androidx.test.espresso.intent.matcher.IntentMatchers.hasAction;
-import static androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent;
-import static androidx.test.espresso.intent.matcher.IntentMatchers.hasData;
-import static androidx.test.espresso.intent.matcher.IntentMatchers.isInternal;
-import static androidx.test.espresso.intent.matcher.IntentMatchers.toPackage;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
-//import static androidx.test.ext.truth.content.IntentSubject.assertThat;
-import static org.hamcrest.Matchers.not;
-import static org.hamcrest.core.AllOf.allOf;
-//import static org.robolectric.annotation.TextLayoutMode.Mode.REALISTIC;
-
-import android.app.Activity;
-import android.content.Intent;
-import android.net.Uri;
-import android.os.Build;
-import androidx.test.espresso.intent.Intents;
 import androidx.test.espresso.intent.rule.IntentsTestRule;
-import androidx.test.ext.junit.runners.AndroidJUnit4;
-import androidx.test.filters.LargeTest;
-import androidx.test.rule.ActivityTestRule;
-import androidx.test.rule.GrantPermissionRule;
-//import com.google.common.collect.Iterables;
-import org.junit.Before;
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-//import org.robolectric.annotation.TextLayoutMode;
 
 
-//AN ATTEMPT WAS MADE - IZZY
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -72,11 +23,7 @@ import org.junit.runner.RunWith;
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
 
-    //Izzy <3's stackoverflow
     // https://stackoverflow.com/questions/37821148/why-cant-i-import-androidjunit4-and-activitytestrule-into-my-unit-test-class
-
-
-
 
     @Rule
     public ActivityScenarioRule<MainActivity> activityRule = new ActivityScenarioRule<>(MainActivity.class);
@@ -85,49 +32,9 @@ public class ExampleInstrumentedTest {
     public IntentsTestRule<MainActivity> mActivityRule = new IntentsTestRule<>(
             MainActivity.class);
 
-    @Before
-    public void stubAllExternalIntents() {
-        // By default Espresso Intents does not stub any Intents. Stubbing needs to be setup before
-        // every test run. In this case all external Intents will be blocked.
-        intending(not(isInternal())).respondWith(new ActivityResult(Activity.RESULT_OK, null));
-    }
-
 
     @Test
     public void validateTextView() {
         onView(withId(R.id.textView)).check(matches(withText("Welcome to SIX")));
     }
-
-    @Test
-    public void clickButtonInventory() {
-
-        onView(withId(R.id.button_full_inventory)).perform(click());
-        assertTrue(true);
-    }
-
-    @Test
-    public void clickButtonPriority() {
-
-
-    }
-
-    @Test
-    public void clickButtonLow() {
-
-
-    }
-
-    @Test
-    public void clickButtonRecipe(){
-
-
-    }
-
-   /* @Test
-    public void useAppContext() {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("edu.gmu.systeminventorydeluxe", appContext.getPackageName());
-    }*/
-
 }
