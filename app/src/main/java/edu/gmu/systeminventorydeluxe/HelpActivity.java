4950aa7196437e96ec67012469face8a5e6d84f5
Isabelle Trenkova <izzytrenkov@gmail.com>
2022-05-03 00:19:38 -0400

    Slowly getting the help menu on point

--- app/src/main/java/edu/gmu/systeminventorydeluxe/HelpActivity.java
+++ app/src/main/java/edu/gmu/systeminventorydeluxe/HelpActivity.java
@@ -1,15 +1,15 @@
 package edu.gmu.systeminventorydeluxe;
 
 import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import com.ms.square.android.expandabletextview.ExpandableTextView;
 
 public class HelpActivity extends AppCompatActivity {
 
     private ExpandableTextView addingtext;
     private ExpandableTextView lowtext;
+    private ExpandableTextView prioritytext;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,15 +20,17 @@ public class HelpActivity extends AppCompatActivity {
 
     private void define() {
 
-        addingtext = (ExpandableTextView)
-                findViewById(R.id.expand_text_view).findViewById(R.id.expand_text_view);
+        addingtext = (ExpandableTextView) findViewById(R.id.expand_text_view);
 
         addingtext.setText(getString(R.string.addItem));
 
         //////////////////
-        //lowtext = (ExpandableTextView) findViewById(R.id.expand_text_view2).findViewById(R.id.expand_text_view);
+        lowtext = (ExpandableTextView) findViewById(R.id.expand_text_view2);
 
-        //lowtext.setText(getString(R.string.lowItem));
+        lowtext.setText(getString(R.string.lowItem));
+
+        prioritytext = (ExpandableTextView) findViewById(R.id.expand_text_view3);
+        prioritytext.setText(getString(R.string.priorityItem));
     }
 
 }
